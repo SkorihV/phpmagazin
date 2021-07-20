937224757469ece8fac106a358dd1ef2ecf3ea6a
Виталий С <skorih.vitalii@gmail.com>
2021-07-20 18:36:37 +0700

    a

--- sql.sql
+++ sql.sql
@@ -7,3 +7,16 @@ GRANT ALL PRIVILEGES ON phpmagazin . * TO 'skorihv'@'localhost';
 
 
 SHOW GRANTS FOR 'skorihv'@'localhost';
+
+
+CREATE TABLE products (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `article` VARCHAR(255),
+    `description` VARCHAR(255) DEFAULT '',
+    `price` INT NOT NULL DEFAULT 0,
+    `currency` VARCHAR(255) DEFAULT 'руб',
+    `amount` INT DEFAULT 0,
+    `weight` INT NOT NULL DEFAULT 0,
+    `weightUnit` VARCHAR(255) NOT NULL DEFAULT 'гр'
+) ENGINE=innoDB DEFAULT CHARSET=utf8;
